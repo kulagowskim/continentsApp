e33dc26b7ed8a0d37b73985ce0719846751a3ceb
kulagowskim <matkulagowski@gmail.com>
2021-06-29 21:31:25 +0200

    Added styles to continents page
    - added styles to continents page
    - added actions to buttons

--- src/pages/Continents.tsx
+++ src/pages/Continents.tsx
@@ -1,22 +1,47 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex, Heading, Text } from "@chakra-ui/react";
 import { useQuery } from "@apollo/client";
+import { useHistory } from "react-router-dom";
 
 import { GET_CONTINENTS } from "./../queries";
 import { IContinentsData } from "../interfaces/IContinent";
+import worldBackground from "./../images/world.svg";
 
 const Continents = () => {
   const { loading, error, data } = useQuery<IContinentsData>(GET_CONTINENTS);
+  const history = useHistory();
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+  const handleClick = () => {
+    history.push("/");
+  };
 
-  return data && data.continents.map(({ code, name }) => (
-    <Flex key={code} flexDirection='column'>
-      <p>
-        {code}: {name}
-      </p>
+  return (
+    <Flex
+      flexDirection="column"
+      bgImage={`url(${worldBackground})`}
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+      h="100vh"
+      alignItems="flex-end"
+      p="100px"
+      justifyContent="space-between"
+      color="white"
+    >
+      <Heading>Continents</Heading>
+      {loading && <Text color="white">Loading...</Text>}
+      {error && <Text color="white">Error ;(</Text>}
+      {data &&
+        data.continents.map(({ code, name }) => (
+          <Flex key={code} flexDirection="column" flexDir="row">
+            <Text fontWeight="bold">{code}:</Text>
+            <Text ml='10px'>{name}</Text>
+          </Flex>
+        ))}
+      <Button w="200px" color='black' onClick={() => handleClick()}>
+        Back to homepage
+      </Button>
     </Flex>
-  ));
+  );
 };
 
 export default Continents;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,18 @@
 import { Button, Flex } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
 import worldBackground from "./../images/world.svg";
 
 const Home = () => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push("/continents");
+  }
   return (
     <Flex
       w="full"
+      h='100vh'
       pr='100px'
       alignItems="center"
       justifyContent="flex-end"
@@ -14,7 +21,7 @@ const Home = () => {
       bgRepeat="no-repeat"
       bgSize="cover"
     >
-      <Button>Show continents</Button>
+      <Button onClick={() => handleClick()}>Show continents</Button>
     </Flex>
   );
 };
